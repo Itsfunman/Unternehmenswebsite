359b5552be78563ee9cbc99d39740870621128ae
christophemanuelgut <fhb211602@fh-vie.ac.at>
2023-11-01 22:32:26 +0100

    Kontakt

---
+++ js/Maps.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([48.2082, 16.3738], 15);  // Koordinaten
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([48.2082, 16.3738]).addTo(map)  // Koordinaten
+    .bindPopup('<strong>DASMÜSSENWIRNOCHÄNDERN GmbH</strong><br>Stephansplatz 1<br>3. Stock, Tür 5<br>1010 Wien<br>Österreich')
+    .openPopup();
