92794608875ddb04fd0d920a884b38a38125450c
christophemanuelgut <fhb211602@fh-vie.ac.at>
2023-11-07 19:25:42 +0100

    Koordinaten DC & Namen

--- js/Maps.js
+++ js/Maps.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([28.2082, 16.3738], 15);  // Koordinaten
+var map = L.map('map').setView([48.2318921, 16.4127015], 15);  // Koordinaten
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([48.2082, 16.3738]).addTo(map)  // Koordinaten
-    .bindPopup('<strong>DASMÜSSENWIRNOCHÄNDERN GmbH</strong><br>Stephansplatz 1<br>3. Stock, Tür 5<br>1010 Wien<br>Österreich')
+    .bindPopup('<strong>TechSolutions Vienna GmbH</strong><br>DC Tower 1, Donau-City-Straße 7<br>3. Etage, Tür 5<br>1220 Wien<br>Österreich')
     .openPopup();
