f967318d24a8396a8b5b3fbfae1f1d1542eee3dd
christophemanuelgut <fhb211602@fh-vie.ac.at>
2023-11-07 19:31:29 +0100

    Koordianten & Namen

--- js/Maps.js
+++ js/Maps.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([48.2082, 16.3738]).addTo(map)  // Koordinaten
-    .bindPopup('<strong>TechSolutions Vienna GmbH</strong><br>DC Tower 1, Donau-City-Straße 7<br>3. Etage, Tür 5<br>1220 Wien<br>Österreich')
+L.marker([48.2318921, 16.4127015]).addTo(map)  // Koordinaten
+    .bindPopup('<strong>TechSolutions Vienna GmbH</strong><br>DC Tower 1<br>30. Stock<br>1220 Wien<br>Österreich')
     .openPopup();
