122ed11978201bba9a063c726fc64464c9bf0404
christophemanuelgut <fhb211602@fh-vie.ac.at>
2023-12-04 13:48:44 +0100

    Local Storage

--- js/bewertungen.js
+++ js/bewertungen.js
@@ -1,34 +1,65 @@
+document.addEventListener('DOMContentLoaded', loadReviewsFromLocalStorage);
+
 function saveReview() {
     var title = document.getElementById('reviewTitle').value.trim();
     var text = document.getElementById('reviewText').value.trim();
-    var stars = document.getElementById('reviewStars').value;
+    var stars = parseInt(document.getElementById('reviewStars').value, 10);
 
-    if (!title || !text || stars < 1 || stars > 5) {
-        alert('Bitte füllen Sie alle Felder korrekt aus. Sternebewertung muss zwischen 1 und 5 liegen.');
+    if (!title || !text || isNaN(stars) || stars < 1 || stars > 5) {
+        alert('Sternebewertung muss zwischen 1 und 5 liegen.');
         return;
     }
 
-    var reviewHTML = '<div class="commonStyles panel review slideInLeft">' +
-                     '<div class="stars">' + '&#9733;'.repeat(stars) + '&#9734;'.repeat(5-stars) + '</div>' +
-                     '<h3 class="boxTitleBewertung">' + title + '</h3>' +
-                     '<p class="text-bewertung">' + text + '</p></div>';
+    var review = {
+        title: title,
+        text: text,
+        stars: stars
+    };
 
-    document.querySelector('.boxes').innerHTML += reviewHTML;
+    addReviewToDOM(review);
+    saveReviewToLocalStorage(review);
+
     document.getElementById('reviewPopup').style.display = 'none';
+    resetReviewForm();
 }
 
+function resetReviewForm() {
+    document.getElementById('reviewTitle').value = '';
+    document.getElementById('reviewText').value = '';
+    document.getElementById('reviewStars').value = '5';
+}
 
 function closeReviewPopup() {
     var closeConfirmation = confirm('Möchten Sie das Bewertungspopup schließen? Alle ungespeicherten Änderungen gehen verloren.');
     if (closeConfirmation) {
         document.getElementById('reviewPopup').style.display = 'none';
-        document.getElementById('reviewTitle').value = '';
-        document.getElementById('reviewText').value = '';
-        document.getElementById('reviewStars').value = '';
+        resetReviewForm();
     }
 }
 
-
 function showReviewPopup() {
     document.getElementById('reviewPopup').style.display = 'block';
+}
+
+function saveReviewToLocalStorage(review) {
+    var reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+    reviews.push(review);
+    localStorage.setItem('reviews', JSON.stringify(reviews));
+}
+
+function loadReviewsFromLocalStorage() {
+    var reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+    reviews.forEach(addReviewToDOM);
+}
+
+function addReviewToDOM(review) {
+    var starsFilled = '&#9733;'.repeat(review.stars);
+    var starsUnfilled = '&#9734;'.repeat(5 - review.stars);
+    var reviewHTML = `<div class="commonStyles panel review slideInLeft">
+                        <div class="stars">${starsFilled}${starsUnfilled}</div>
+                        <h3 class="boxTitleBewertung">${review.title}</h3>
+                        <p class="text-bewertung">${review.text}</p>
+                      </div>`;
+
+    document.querySelector('.boxes').innerHTML += reviewHTML;
 }
